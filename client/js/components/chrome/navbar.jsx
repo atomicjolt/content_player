6c63346541e684e0341f11a2f2adc88e0a1dbd21
bensmith <benhsmith13@gmail.com>
2016-07-15 14:07:49 -0600

    Nabber and stuff

---
+++ client/js/components/chrome/navbar.jsx
@@ -0,0 +1,26 @@
+"use stict";
+
+import React            from 'react';
+
+export default (props)=>{
+
+  const styles = {
+    container: {
+      position: 'fixed',
+      top: '0px',
+      left: '0px',
+      width: '100%',
+      height: '72px',
+      backgroundColor: '#999999',
+      boxShadow: '1px 1px 1px gray'
+    },
+    logo: {
+      width: '100px',
+      padding: '10px'
+    }
+  };
+
+  return <div style={styles.container}>
+    <img src="http://supplychainmit.com/wp-content/uploads/2015/04/MIT-logo.png" style={styles.logo}/>
+  </div>
+}
--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -2,15 +2,31 @@
 
 import React                    from 'react';
 import assets                   from '../libs/assets';
+import Navbar                   from './chrome/navbar.jsx';
+import BookOverview             from './book/book_overview';
 
 class Home extends React.Component {
 
+  getStyles(){
+    return{
+      body: {
+        position: 'fixed',
+        top: '0px',
+        left: '0px',
+        width: '100%',
+        height: '100%',
+      }
+    }
+  }
+
   render(){
-
     const img = assets("./images/atomicjolt.jpg");
+    const styles = this.getStyles();
 
-    return<div>
-    <img src={img} />
+    return<div style={styles.body}>
+      <Navbar/>
+      <BookOverview />
+      <img src={img} />
     </div>;
   }
 
