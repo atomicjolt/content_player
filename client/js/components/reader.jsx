2e6ea850a836a458948c013a00da3e08d67fd111
James Carbine <jamescarbine@gmail.com>
2016-07-15 15:57:35 -0600

    Add page component Move chrome from reader to index

--- client/js/components/reader.jsx
+++ client/js/components/reader.jsx
@@ -1,49 +1,14 @@
 "use strict";
 
 import React                    from 'react';
-import assets                   from '../libs/assets';
-import Chrome                   from './chrome/chrome.jsx';
 
-class Home extends React.Component {
-
-  getStyles(){
-    return{
-      body: {
-        position: 'fixed',
-        top: '0px',
-        left: '0px',
-        width: '100%',
-        height: '100%',
-      },
-      logo: {
-        position: 'absolute',
-        bottom: '20px',
-        right: '20px'
-      },
-      content: {
-        position: 'absolute',
-        top: '72px',
-        left: '250px',
-        width: 'calc(100% - 292px)',
-        height: 'calc(100% - 72px)',
-        backgroundColor: 'lightGrey'
-      }
-    }
-  }
+class Reader extends React.Component {
 
   render(){
-    const img = assets("./images/atomicjolt.jpg");
-    const styles = this.getStyles();
-
-    return<div style={styles.body}>
-      <Chrome />
-      <div style={styles.content}>
-        {this.props.children}
-      </div>
-      <img src={img} style={styles.logo}/>
+    return<div>
     </div>;
   }
 
 }
 
-export { Home as default };
+export { Reader as default };
