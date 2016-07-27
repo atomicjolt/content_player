6f19082f53ec6318661437522436c0cb93f0f723
Nick <nick.benoit14@gmail.com>
2016-07-27 16:16:19 -0600

    remove more page load

--- client/js/reducers/content.js
+++ client/js/reducers/content.js
@@ -15,12 +15,6 @@ export default (state = {}, action) => {
       state.title = action.tocDoc.docTitle;
       return _.merge({}, {...state});
 
-    case ContentConstants.LOAD_PAGE_DONE:
-      let newEntry = {id: action.id, body: action.pageContent};
-      if(state.pages){ state.pages.push(newEntry); }
-      else { state.pages = [newEntry]; }
-      return _.merge({}, {...state});
-
     default:
       return state;
   }
