c843f0dd95a75ba74d0b580af0a4374313bfd5d3
Nick <nick.benoit14@gmail.com>
2016-07-28 11:30:20 -0600

    add action and reducer for toggle sidebar

--- client/js/reducers/application.js
+++ client/js/reducers/application.js
@@ -1,11 +1,18 @@
 "use strict";
 
-const initialState =  {};
+import {Constants as ApplicationConstants} from '../actions/application';
+
+const initialState =  {
+  sidebarOpen: true
+};
 
 export default (state = initialState, action) => {
 
   switch(action.type){
-
+    case ApplicationConstants.TOGGLE_SIDEBAR:
+      state.sidebarOpen = !state.sidebarOpen
+      return Object.assign({}, state);
+      break;
     default:
       return state;
   }
