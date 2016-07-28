94d8a78b891a52c65f5816f0241f72c811b0e1e7
Nick <nick.benoit14@gmail.com>
2016-07-28 10:48:28 -0600

    remove inline styles from page and sidebar

--- client/js/components/book/page.jsx
+++ client/js/components/book/page.jsx
@@ -15,60 +15,21 @@ const select = (state) => {
 
 @connect(select, ContentActions)
 export default class Page extends React.Component {
-  constructor(props){
-    super();
-  }
-
-  getStyles(){
-    return{
-      logo: {
-        position: 'absolute',
-        bottom: '20px',
-        right: '20px'
-      },
-      content: {
-        position: 'relative',
-        left: '300px',
-        top: '80px'
-      },
-      iframe: {
-        margin: '0 auto',
-        padding: 0,
-        border: 'none',
-        height: 'calc(100% - 72px)',
-        width: '90%',
-        display: 'block',
-        position: 'relative',
-        top: '72px',
-      },
-      container: {
-        height: '100%',
-        width: '100%'
-      }
-    };
-  }
 
   iframe(props){
     var current = _.find(
       props.tableOfContents,
       (item) => item.id == this.props.params.pageId
     );
-    var styles = this.getStyles();
     if(!current){return;}
     return (
-      <iframe
-        style={styles.iframe}
-        src={`pubs/${props.contentName}/OEBPS/${current.content}`} />
+      <iframe src={`pubs/${props.contentName}/OEBPS/${current.content}`} />
     );
   }
 
   render(){
-    const styles = this.getStyles();
-
-
-
     return (
-      <div style={styles.container}>
+      <div className="c-page">
         {this.iframe(this.props)}
       </div>
     );
--- client/js/components/chrome/sidebar.jsx
+++ client/js/components/chrome/sidebar.jsx
@@ -15,40 +15,7 @@ const select = (state) => {
 @connect(select)
 export default class Sidebar extends React.Component{
 
-  getStyles(){
-    return {
-      container: {
-        position: 'fixed',
-        top: '72px',
-        left: this.props.sidebarOpen ? '0px' : '-300px',
-        width: '292px',
-        height: 'calc(100% - 72px)',
-        overflowY: 'scroll',
-        borderRight: '3px solid deepPink',
-        zIndex: '2',
-        transition: 'all .4s ease',
-        alignItems: 'stretch',
-        backgroundColor: '#4a4a4a'
-      },
-      unit: {
-        marginTop: '40px',
-        paddingLeft: '20px',
-        fontSize: '.7rem',
-        color: '#fff',
-        fontWeight: 300,
-        letterSpacing: '1.15px',
-        textTransform: 'uppercase'
-      },
-      subject:{
-        paddingLeft: '20px',
-        marginBottom: '10px',
-        color: '#fff',
-        fontWeight: 300,
-        fontSize: '.875rem',
-        textTransform: 'uppercase'
-      }
-    };
-  }
+
 
   tableOfContents(){
     return _.map(this.props.tableOfContents, (item)=>{
@@ -57,11 +24,11 @@ export default class Sidebar extends React.Component{
   }
 
   render(){
-    const styles = this.getStyles();
 
-    return <div style={styles.container}>
-      <div style={styles.unit}>GRADE - UNIT</div>
-      <div style={styles.subject}>{this.props.title || 'LESSON SUBJECT'}</div>
+    var sidebarClass = this.props.sidebarOpen ? "c-sidebar c-sidebar--open" : "c-sidebar";
+    return <div className={sidebarClass}>
+      <div className="unit">GRADE - UNIT</div>
+      <div className="subject">{this.props.title || 'LESSON SUBJECT'}</div>
       {this.tableOfContents()}
     </div>;
   }
