c5c95a457212ef709bbd15d0c951fa7d0611cd7c
Nick <nick.benoit14@gmail.com>
2016-08-01 13:17:40 -0600

    form relative epub path

--- client/js/middleware/epub.js
+++ client/js/middleware/epub.js
@@ -1,12 +1,19 @@
 import api         from "../libs/api";
 import { DONE }    from "../constants/wrapper";
 import { parse }   from "../libs/parser";
+import _           from 'lodash';
 
 
 function getToc(contentDoc){
   var tocID = contentDoc.spine.toc;
   var toc = contentDoc.manifest.filter((item) => {if(item.id == tocID) return true;});
   return toc[0];
+}
+
+function getRelativePath(manifest){
+  let segments = manifest.rootfiles['full-path'].split('/');
+  segments.splice(-1,1); // Remove last element
+  return `${segments.join('/')}`;
 }
 
 const EPUB = store => next => action => {
@@ -20,6 +27,8 @@ const EPUB = store => next => action => {
         let parser  = new DOMParser();
         let xmlDoc  = parser.parseFromString(response.text,"text/xml");
         let manifest = parse(xmlDoc);
+        let relativePath = getRelativePath(manifest);
+
         let contentPromise = api.execRequest(method, `pubs/${name}/${manifest.rootfiles["full-path"]}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
         contentPromise.then((response, error)=>{
           let contentParser  = new DOMParser();
@@ -28,8 +37,7 @@ const EPUB = store => next => action => {
           // fix if we really do this.
           // TODO How to tell if ncx or not
           let toc = getToc(contentDoc);
-
-          let tocPromise = api.execRequest(method, `pubs/${name}/OEBPS/og${toc.href}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
+          let tocPromise = api.execRequest(method, `pubs/${name}/${relativePath}/${toc.href}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
           tocPromise.then((response, error)=>{
             let tocParser  = new DOMParser();
             let tocXml  = tocParser.parseFromString(response.text,"text/xml");
