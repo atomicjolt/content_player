799ced88dbd63cde1876bbccb6a681de29576aea
Nick <nick.benoit14@gmail.com>
2016-08-01 16:36:55 -0600

    refactor requestToc

--- client/js/middleware/epub.js
+++ client/js/middleware/epub.js
@@ -1,4 +1,5 @@
 import api         from "../libs/api";
+import Network    from '../constants/network';
 import { DONE }    from "../constants/wrapper";
 import { parse }   from "../libs/parser";
 import _           from 'lodash';
@@ -21,57 +22,98 @@ function getToc(contentDoc){
   return toc[0];
 }
 
-function handleResponse(response, handleItem){
+function handleResponse(response, handleItem, params = []){
   let parser  = new DOMParser();
   let xmlDoc  = parser.parseFromString(response.text,"text/xml");
   let item = parse(xmlDoc);
-  if(handleItem){return handleItem(item);}
+  if(handleItem){return handleItem(item, ...params);}
 }
 
-const EPUB = store => next => action => {
-
-  function requestToc(method, name, params, body){
-    const state = store.getState();
-    const metaPromise = api.execRequest(method, `pubs/${name}/META-INF/container.xml`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
-    if(metaPromise){
-      metaPromise.then((response) => {
-        var contentPath;
-        var contentPromise = handleResponse(response, (item) => {
-          // Path to epub content
-          contentPath = `pubs/${name}`;
-          let relativePath = getRelativePath(item);
-
-          // if there is a relative path within the epub to the content then append it.
-          if(!_.isEmpty(relativePath)){contentPath += `/${relativePath}`;}
-          return api.execRequest(method, `pubs/${name}/${item.rootfiles["full-path"]}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
-        });
-
-        contentPromise.then((response) => {
-
-          var tocPromise = handleResponse(response, (item) => {
-            let toc = getToc(item);
-            return api.execRequest(method, `${contentPath}/${toc.href}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
-          });
-
-          tocPromise.then((response) => {
-            handleResponse(response, (item) => {
-              let tableOfContents = _.isArray(item.navMap) ? item.navMap : [item.navMap];
-              store.dispatch({
-                type:     action.type + DONE,
-                tableOfContents,
-                original: action,
-                tocDoc: item,
-                contentPath,
-              });
-            });
-          });
-        });
-      });
-    }
+function requestContainer(state, epubUrl, next){
+  const metaPromise = api.execRequest(Network.GET, `${epubUrl}/META-INF/container.xml`, state.settings.apiUrl, state.jwt, state.settings.csrfToken);
+  if(metaPromise){
+    metaPromise.then((response) => {
+      handleResponse(response, next, [epubUrl]);
+    });
   }
+}
+
+function requestRootFile(state, container, epubUrl, epubPath, next){
+  var rootfile = api.execRequest(Network.GET, `${epubUrl}/${container.rootfiles["full-path"]}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken);
+  if(!_.isEmpty(epubPath)){epubUrl += `/${epubPath}`;}
+  rootfile.then((response) => {
+    handleResponse(response, next, [epubUrl]);
+  });
+}
+
+function requestTableOfContents(state, manifest, epubUrl, next){
+  var tocID = manifest.spine.toc;
+  var toc = manifest.manifest.filter((item) => {if(item.id == tocID) return true;})[0];
+  var tocPromise = api.execRequest(Network.GET, `${epubUrl}/${toc.href}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken);
+  tocPromise.then((response) => {
+    handleResponse(response, next, [epubUrl]);
+  });
+}
+
+
+const EPUB = store => next => action => {
+  // function requestToc(method, name, params, body){
+  //   const state = store.getState();
+  //   const metaPromise = api.execRequest(method, `pubs/${name}/META-INF/container.xml`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
+  //   if(metaPromise){
+  //     metaPromise.then((response) => {
+  //       var contentPath;
+  //       var contentPromise = handleResponse(response, (item) => {
+  //         // Path to epub content
+  //         contentPath = `pubs/${name}`;
+  //         let relativePath = getRelativePath(item);
+  //
+  //         // if there is a relative path within the epub to the content then append it.
+  //         if(!_.isEmpty(relativePath)){contentPath += `/${relativePath}`;}
+  //         return api.execRequest(method, `pubs/${name}/${item.rootfiles["full-path"]}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
+  //       });
+  //
+  //       contentPromise.then((response) => {
+  //
+  //         var tocPromise = handleResponse(response, (item) => {
+  //           let toc = getToc(item);
+  //           return api.execRequest(method, `${contentPath}/${toc.href}`, state.settings.apiUrl, state.jwt, state.settings.csrfToken, params, body);
+  //         });
+  //
+  //         tocPromise.then((response) => {
+  //           handleResponse(response, (item) => {
+  //             let tableOfContents = _.isArray(item.navMap) ? item.navMap : [item.navMap];
+  //             store.dispatch({
+  //               type:     action.type + DONE,
+  //               tableOfContents,
+  //               original: action,
+  //               tocDoc: item,
+  //               contentPath,
+  //             });
+  //           });
+  //         });
+  //       });
+  //     });
+  //   }
+  // }
 
   if(action.epubMethod){
-    requestToc(action.epubMethod, action.name, action.params, action.body);
+    // requestToc(action.epubMethod, action.name, action.params, action.body);
+    const state = store.getState();
+    requestContainer(state, `pubs/${action.name}`, (item, epubUrl) => {
+      requestRootFile(state, item, epubUrl, getRelativePath(item), (item, epubUrl) => {
+        requestTableOfContents(state, item, epubUrl, (item, epubUrl) => {
+          let tableOfContents = _.isArray(item.navMap) ? item.navMap : [item.navMap];
+          store.dispatch({
+            type:     action.type + DONE,
+            tableOfContents,
+            original: action,
+            tocDoc: item,
+            contentPath: epubUrl,
+          });
+        })
+      })
+    });
   }
 
 
