c925bd82607d5764dd8dac0a9c3b35c4f2475909
Nick <nick.benoit14@gmail.com>
2016-08-02 09:11:36 -0600

    added handleItem spec

--- client/js/middleware/epub.spec.js
+++ client/js/middleware/epub.spec.js
@@ -2,7 +2,39 @@ import * as Epub from './epub';
 
 
 describe('epub middleware', () => {
-  describe('handleResponse', () => {});
+  describe('handleResponse', () => {
+    const params = {
+
+      handleItem:(item, param1, param2) => {
+      },
+      response: {
+        text: `<?xml version="1.0" encoding="UTF-8"?>
+            <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+              <rootfiles>
+                  <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
+             </rootfiles>
+          </container>`
+      }
+    };
+
+    const parsedItem = {
+      "rootfiles": {
+        "full-path": "OEBPS/content.opf",
+        "media-type": "application/oebps-package+xml"
+      },
+      "xmlns": "urn:oasis:names:tc:opendocument:xmlns:container",
+      "version": "1.0"
+    };
+
+    it("calls handleItem", () => {
+      spyOn(params, 'handleItem');
+      const callbackParams =  [1,2,3];
+      Epub.handleResponse(params.response, params.handleItem, callbackParams);
+
+      expect(params.handleItem).toHaveBeenCalledWith(parsedItem, ...callbackParams);
+    });
+  });
+
   describe('getRelativePath', () => {});
   describe('requestContainer', () => {});
   describe('requestRootFile', () => {});
