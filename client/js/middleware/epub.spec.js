09685995d9db940430e19f9295919340935ed3d0
Nick <nick.benoit14@gmail.com>
2016-08-02 08:54:07 -0600

    add specs

--- client/js/middleware/epub.spec.js
+++ client/js/middleware/epub.spec.js
@@ -2,5 +2,9 @@ import * as Epub from './epub';
 
 
 describe('epub middleware', () => {
-  
+  describe('handleResponse', () => {});
+  describe('getRelativePath', () => {});
+  describe('requestContainer', () => {});
+  describe('requestRootFile', () => {});
+  describe('requestTableOfContents', () => {});
 });
