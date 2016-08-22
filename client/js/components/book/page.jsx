8e9b09029b90b805d5b89e5ede70076ffc62a652
David Owen <dsowen@fugue88.ws>
2016-08-22 15:55:39 -0600

    Set the locale in the assessment player

--- client/js/components/book/page.jsx
+++ client/js/components/book/page.jsx
@@ -1,10 +1,11 @@
 "use strict";
 
-import React                    from "react";
-import assets                   from '../../libs/assets';
-import * as ContentActions      from '../../actions/content';
-import _                        from 'lodash';
-import { connect }              from "react-redux";
+import _            from "lodash";
+import React        from "react";
+import { connect }  from "react-redux";
+
+import * as ContentActions  from "../../actions/content";
+import assets               from "../../libs/assets";
 
 const select = (state) => {
   return {
@@ -17,18 +18,38 @@ const select = (state) => {
 
 export class Page extends React.Component {
 
-  iframe(props){
+  onMessage(message) {
+    // Inconveniently, we don't seem to be able to locate the
+    // assessment-player's iframe inside the content's iframe.  Conventiently,
+    // the assessment-player sends a message up to us to indicate its available
+    // locales.  Although we ignore the available locales, we use that message's
+    // source to target a message back down to the assessment-player.
+    const data = message.data;
+    const type = data.open_assessments_msg;
+    switch(type) {
+      case "open_assessments_available_locales":
+        message.source.postMessage({
+          open_assessments_msg: "open_assessments_set_locale",
+          locale: "hi"
+        }, "*");
+        break;
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("message", (e) => this.onMessage(e), false);
+  }
+
+  iframe(props) {
     var current = _.find(
       props.tableOfContents,
       (item) => item.id == props.params.pageId
     );
-    if(!current){return;}
-    return (
-      <iframe src={`${props.contentPath}/${current.content}`} />
-    );
+    if(!current) { return; }
+    return <iframe src={`${props.contentPath}/${current.content}`} />;
   }
 
-  render(){
+  render() {
     var lastModified = this.props.tocMeta.lastModified;
     var footerText = lastModified ? `CLIx release date: ${lastModified}` : undefined;
     return (
